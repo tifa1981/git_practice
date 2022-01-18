c493510ed61a79ef3e6c15059630b4a554a57608
YanchunZhou <tifa1981@gmail.com>
2022-01-18 10:54:10 -0500

    3rd commit

--- src/Practice1.java
+++ src/Practice1.java
@@ -4,5 +4,6 @@ public class Practice1 {
     // If a subclass defines a static method with the same signature as the static method in the superclass,
     // then the method in the subclass hides the one in the superclass.
     //jian
+// jian jianjiaj
 
 }
